2c64efe7e2ea258a628a5e7a9a2fa036a9cab327
Phai <norapat_c@cmu.ac.th>
2024-02-23 21:28:23 +0700

    updated

--- SpringSocketDemo/src/main/java/com/websocket/demo/chat/ChatMessage.java
+++ SpringSocketDemo/src/main/java/com/websocket/demo/chat/ChatMessage.java
@@ -3,12 +3,24 @@ package com.websocket.demo.chat;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
 @Builder
+@Setter
 public class ChatMessage {
     private String content;
     private String timestamp;
     private String sender;
     private MessageType type;
+    private int chatCC;
+    public static int CC;
+    public static int increaseCC(){
+        CC += 1;
+        return CC;
+    }
+    public static int decreaseCC(){
+        CC -= 1;
+        return CC;
+    }
 }
